5f8c4c4ab116fba8d30912c3e33c43635e7bd64f
Leon <leongermdp@gmail.com>
2023-01-17 19:28:36 -0300

    Actualizacion Fake API json-server

--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -2,6 +2,10 @@
 
 const listaProductos = ()=> fetch("http://localhost:3000/producto").then(respuesta => respuesta.json())
 
-export const productoServices = {
+
+export const productoServices = 
+{
     listaProductos
 }
+
+//Me devuelve db.json a travez del servidor json.
